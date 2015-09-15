acbefc37446860c26025be6b23d18b1d76c82626
Matt Duncan <busman84@gmail.com>
2015-09-15 08:56:36 -0500

    Addind Never Ending story morning exercise

---
+++ w04/d02/never_ending_story/never_ending_story.rb
@@ -0,0 +1,10 @@
+story = [
+  'Fry is born without a delta brainwave',
+  'As a result, Fry is cryogenically frozen until 2999',
+  'In the future, Fry works on a spaceship',
+  'The spaceship hits a wormhole and crashes in 1941 Roswell, NM',
+  'Fry encounters his grandmother, then inadvertently sires his father',
+  "Fry's best friend becomes a robot named Bender when they meet in a suicide booth",
+  'Fry falls in love with Leela, a one eyed mutant from the sewers',
+  'Fry and Leela save Bender from robot hell after his addiction to electricity'
+]
