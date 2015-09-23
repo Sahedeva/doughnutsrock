52940597d3545e9b5eb9f5a2df6ae733badd15b0
Mike Dang <mike.dang@ga.co>
2015-09-23 13:33:04 -0500

    Adding code from class

---
+++ w05/d03/PhotoApp/db/migrate/20150923162929_create_photos.rb
@@ -0,0 +1,13 @@
+class CreatePhotos < ActiveRecord::Migration
+  def change
+    create_table :photos do |t|
+      t.text :path, null: false
+      t.text :caption 
+      t.integer :height
+      t.integer :width
+      t.text :title 
+
+      t.timestamps null: false
+    end
+  end
+end
