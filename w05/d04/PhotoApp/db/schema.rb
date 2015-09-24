c32264ea080eedafd0bda7f5c152e4d0a1812526
Mike Dang <mike.dang@ga.co>
2015-09-24 15:09:57 -0500

    Adding the code from today's lecture

--- w05/d04/PhotoApp/db/schema.rb
+++ w05/d04/PhotoApp/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150923202653) do
+ActiveRecord::Schema.define(version: 20150924162834) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,15 @@ ActiveRecord::Schema.define(version: 20150923202653) do
     t.text     "name"
   end
 
-  add_index "photos", ["title"], name: "index_photos_on_title", using: :btree
+  create_table "users", force: :cascade do |t|
+    t.text     "first_name",      null: false
+    t.text     "last_name",       null: false
+    t.text     "email",           null: false
+    t.text     "password_digest", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
