dcfcc2df7e6c59bad46c4efcafba70d801973d67
Sean Shannon <sean.shannon@ga.co>
2015-10-07 16:59:30 -0500

    movie favs app

--- w07/d03/movie_favs/app/assets/javascripts/application.js
+++ w07/d03/movie_favs/app/assets/javascripts/application.js
@@ -12,22 +12,25 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
-
+//= require jquery-ui
 
 $(function() {
+
+  $.ajax({
+    url: "https://twitter.com/i/timeline?composed_count=0&include_available_features=1&include_entities=1&include_new_items_bar=true&interval=10000&last_note_ts=1444232758&latent_count=0&min_position=651869076495601664"
+  });
 
   $("#search").on("click", function(evt) {
 
     $.ajax({
       url: "http://www.omdbapi.com",
       data: {
-        s: "Danger Mouse",
-        type: "series"
+        s: $("#search-term").val()
       },
       dataType: "json",
       method: "GET",
       success: function(data, textStatus, jqXHR) {
+        $("#results").html('');
         for (var i = 0; i < data['Search'].length; i++) {
           $("#results").append("<li>"+ data['Search'][i]['Title'] + ", " + data['Search'][i]['Year'] +"</li>")
         }
@@ -35,5 +38,28 @@ $(function() {
     });
 
   });
+
+
+  // $("#search-term").autocomplete({
+  //   minLength: 3,
+  //   position: { my: "left top", at: "left bottom" },
+  //   source: function (request, response) {
+  //     var titles = [];
+  //     $.ajax({
+  //       url: "http://www.omdbapi.com",
+  //       data: {
+  //         s: request.term
+  //       },
+  //       dataType: "json",
+  //       method: "GET",
+  //       success: function(data, textStatus, jqXHR) {
+  //         for (var i = 0; i < data['Search'].length; i++) {
+  //           titles.push(data['Search'][i]['Title']);
+  //         }
+  //         response(titles);
+  //       }
+  //     });
+  //   }
+  // });
 
 });
--- w07/d03/movie_favs/config/routes.rb
+++ w07/d03/movie_favs/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   root "application#index"
+
   resources :favorites, :except => [:edit, :new, :update]
 
   # The priority is based upon order of creation: first created -> highest priority.
