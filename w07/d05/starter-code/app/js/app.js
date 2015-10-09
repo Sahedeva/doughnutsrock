32ac34d42ffdaa80283ff3038de3cb8406f0a617
Robert Weilbaecher <robertweilbaecher@hotmail.com>
2015-10-09 10:29:22 -0500

    first commit

--- w07/d05/starter-code/app/js/app.js
+++ w07/d05/starter-code/app/js/app.js
@@ -1,4 +1,16 @@
 // jQuery Document Ready
 $(function() {
-  
+  $.ajax({
+      url: "http://api.doughnuts.ga/doughnuts",
+      dataType: "jsonp",
+      method: "GET",
+      success: function(data, textStatus, jqXHR) {
+      	console.log(data);
+      	console.log(data[0]['flavor']);
+      	for (var i = 0; i < data.length; i++) {
+      	$("#doughnuts").append("<li id='"+data[i]['id']+"'> "+data[i]['style']+" "+data[i]['flavor']+"</li>");
+      	}
+      }
+    });
+
 });
